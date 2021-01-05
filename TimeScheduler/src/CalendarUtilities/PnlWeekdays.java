2c6895334395ee35b9c06b0c87e55c4b9ed952ac
Benny <Benny@192.168.192.37>
2021-01-05 20:14:59 +0100

    hi

--- TimeScheduler/src/CalendarUtilities/PnlWeekdays.java
+++ TimeScheduler/src/CalendarUtilities/PnlWeekdays.java
@@ -5,7 +5,9 @@
  */
 package CalendarUtilities;
 
+import java.awt.event.ComponentAdapter;
 import java.util.ArrayList;
+import javax.swing.event.AncestorEvent;
 
 /**
  *
@@ -16,16 +18,35 @@ public class PnlWeekdays extends javax.swing.JPanel{
     //Weekdays
     private final String [] weekdays = {"Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday", "Sunday"};
     private final java.util.ArrayList<javax.swing.JLabel> lblList;
+    private final int height;
     
     public PnlWeekdays(){
         super();
         
         //initialize global variables
         this.lblList = new ArrayList<>();
+        this.height = 20;
         
         //some settings
         this.setLayout(null);
         this.setBackground(java.awt.Color.decode("0x0088FF"));
+        
+        //add listener
+        this.addComponentListener(new ComponentAdapter() {
+            
+            public void componentResized(java.awt.event.ComponentEvent evt){
+                setLabelPositions();
+            }
+            
+        });
+        
+        this.addHierarchyBoundsListener(new java.awt.event.HierarchyBoundsListener() {
+            public void ancestorMoved(java.awt.event.HierarchyEvent evt) {
+            }
+            public void ancestorResized(java.awt.event.HierarchyEvent evt) {      
+                resize();               
+            }
+        });
     }
     
     private void createWeekdayLabels(){
@@ -43,8 +64,18 @@ public class PnlWeekdays extends javax.swing.JPanel{
         }
     }
     
-    private void refreshLabels(){
+    private void setLabelPositions(){
+        
+        for (int i = 0; i < this.lblList.size(); i++){
+            
+            lblList.get(i).setBounds((int)(Math.ceil(this.getHeight() / 3)), i * (int)(Math.ceil(this.getWidth() / 14) - this.getWidth() / 28), 20, 20);
+            
+        }
+                
         
     }
     
+    private void resize(){
+        this.setBounds(0, 0, this.getParent().getWidth(), this.height);
+    }
 }
