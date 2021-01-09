165d4ab77ef6e050f70cbc02b4b46e96ed35c0d7
Benny <Benny@192.168.192.37>
2021-01-09 16:59:04 +0100

    Das was Vadim sagt

--- TimeScheduler/src/handlers/ExportHandler.java
+++ TimeScheduler/src/handlers/ExportHandler.java
@@ -5,9 +5,13 @@
  */
 package handlers;
 
+import classes.Event;
+import classes.User;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  *
@@ -15,35 +19,77 @@ import java.io.IOException;
  */
 public class ExportHandler {
 
-    public ExportHandler() {
-        
-        classes.Event evt = new classes.Event();
-        
-        
+    public void writeWeeklySchedule(java.util.ArrayList<classes.Event> eList, java.time.LocalDate start, java.time.LocalDate end, String path) {
+
+        if (!this.checkIfPathIsValid(path)) {
+            System.out.println(path + " is not valid!");
+        }
+
+        path += "\\Weekly Schedule.txt";
+
+        this.createTxtFile(path);
+
         try {
-            File myObj = new File("C:\\Users\\Benny\\Desktop\\test.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
+            FileWriter myWriter = new FileWriter(path);
+
+            myWriter.write("Weekly Schedule from " + start.getDayOfMonth() + "." + start.getMonth() + "." + start.getYear() + " - "
+                    + end.getDayOfMonth() + "." + end.getMonth() + "." + end.getYear());
+            myWriter.write("\n\n");
+
+            for (classes.Event e : eList) {
+
+                myWriter.write("Event name:\t\t" + e.getName());
+                myWriter.write("\n");
+                myWriter.write("Date:\t\t\t" + e.getDate().getDayOfMonth() + "." + e.getDate().getMonth() + "." + e.getDate().getYear() + " " + e.getDate().getHour() + ":" + e.getDate().getMinute());
+                myWriter.write("\n");
+                myWriter.write("Location:\t\t" + e.getLocation());
+                myWriter.write("\n");
+                myWriter.write("Duration:\t\t" + e.getDuration() + " Minutes");
+                myWriter.write("\n");
+                myWriter.write("Organisator:\t\t" + e.getOrganisator().getFirstName() + " " + e.getOrganisator().getLastName());
+                myWriter.write("\n");
+                myWriter.write("Organisator email:\t" + e.getOrganisator().getEmail());
+                myWriter.write("\n");
+                myWriter.write("Participants:\t\t" + e.getParticipants().size());
+                myWriter.write("\n");
+                myWriter.write("Priority:\t\t" + e.getPriority());
+                myWriter.write("\n");
+                myWriter.write("----------------------------------------");
+                myWriter.write("\n");
+
             }
+
+            System.out.println("Successfully write to Weekly Schedule file");
+
+            myWriter.close();
         } catch (IOException e) {
-            System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
-        try {
-            FileWriter myWriter = new FileWriter("C:\\Users\\Benny\\Desktop\\test.txt");
-            
-            //Write
-            
-            //Write
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
     }
 
+    private boolean checkIfPathIsValid(String path) {
+        File theDir = new File(path);
+        if (theDir.exists()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void createTxtFile(String path) {
+        try {
+            File report = new File(path);
+            if (report.createNewFile()) {
+                System.out.println("Weekly report created: " + report.getName());
+            } else {
+                System.out.println("Weekly Report already exists.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ExportHandler() {
+
+    }
 }
