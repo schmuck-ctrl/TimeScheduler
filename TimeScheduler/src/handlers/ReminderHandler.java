78ec103de2021d1b178a59256ae24b1bed6542e1
Benny <Benny@192.168.192.37>
2021-01-13 22:58:30 +0100

    Added ReminderHandler JavaDoc

--- TimeScheduler/src/handlers/ReminderHandler.java
+++ TimeScheduler/src/handlers/ReminderHandler.java
@@ -6,13 +6,20 @@
 package handlers;
 
 /**
- *
+ * ReminderHandler inherits Thread
+ * 
  * @author Benny
  */
 public class ReminderHandler extends Thread {
 
     classes.Operator op;
 
+    /**
+     * Creates a new Instance of ReminderHandler
+     * 
+     * @param op Classes.Operator to get all Appointments from the Operator
+     * @throws Exception Throws Exception if op is null
+     */
     public ReminderHandler(classes.Operator op) throws Exception {
         if (op != null) {
             this.op = op;
@@ -21,46 +28,56 @@ public class ReminderHandler extends Thread {
         }
     }
 
+    /**
+     * run() is called if ReminderHandler.start() is called.
+     */
     public void run() {
-
+        
+        //create new List for Events, where the user will be reminded
         java.util.ArrayList<classes.Event> toBeRemindedList = new java.util.ArrayList<classes.Event>();
 
         DatabaseHandler db = new DatabaseHandler();
 
         while (true) {
 
+            //Get all Events where current User hasn't been reminded from Database
             java.util.ArrayList<classes.Event> reminderList = db.getNotNotifiedEventsFromUser(this.op.getUserId());
+            //clear toBeReminded list so user doesn't get reminded twice or more 
             toBeRemindedList.clear();
 
             try {
+                //Wait 1 second until continue
                 Thread.sleep(1000);
 
                 for (classes.Event evt : reminderList) {
 
+                    //if event date is after today, otherwise event is already over, so there is no consideration at all
                     if (evt.getDate().isAfter(java.time.LocalDateTime.now())) {
-
+                        //when reminder date is before today, there was no reminder OR reminder date is equal to date from now
                         if (evt.getReminder().isBefore(java.time.LocalDateTime.now()) || evt.getReminder().isEqual(java.time.LocalDateTime.now())) {
-
+                            //add event to be reminded to list
                             toBeRemindedList.add(evt);
-
+                            //sets the Participant.P_notified to 1, so the user won't be reminded more than once
                             db.setUserNotified(evt.getID(), this.op.getUserId());
                         }
                     }
                 }
 
+                //build the message string for multiple reminders on same time
                 String message = "";
                 for (classes.Event evt : toBeRemindedList) {
+                    //build message string
                     message += evt.getName() + ": " + evt.getDate().getDayOfMonth() + "/" + evt.getDate().getMonthValue() + 
                             "/" + evt.getDate().getYear() + " | " + (evt.getDate().getHour() > 10 ? evt.getDate().getHour() : ("0" + evt.getDate().getHour())) +
                             ":" + (evt.getDate().getMinute() > 10 ? evt.getDate().getMinute() : ("0" + evt.getDate().getMinute()));
                     message += "\n";
                 }
 
+                //when toBeReminded is not empty, the dialog will be set, otherwise there will be no dialog.
                 if (!toBeRemindedList.isEmpty()) {
                     javax.swing.JOptionPane.showMessageDialog(forms.FrmMain.getInstance(), message, "Reminder", javax.swing.JOptionPane.INFORMATION_MESSAGE);
                 }
 
-                //call FrmMain function for reminding
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
