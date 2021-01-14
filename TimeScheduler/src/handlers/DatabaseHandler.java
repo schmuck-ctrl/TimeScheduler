b376aca8ac83a7aa5fbce782c6beeb6552c8b31d
Joshua <joshu@192.168.0.248>
2021-01-14 02:21:46 +0100

    fixxed notifications

--- TimeScheduler/src/handlers/DatabaseHandler.java
+++ TimeScheduler/src/handlers/DatabaseHandler.java
@@ -292,7 +292,7 @@ public class DatabaseHandler {
 
     public ArrayList<Event> getNotNotifiedEventsFromUser(int userID) {
         ArrayList<Integer> eventIDs = getEventIDsNotNotified(userID);
-        String notNotified = "SELECT * FROM eventdetails WHERE ED_eventID IN(" + listToString(eventIDs) + ");";
+        String notNotified = "SELECT * FROM eventdetails WHERE ED_eventID IN(" + listToString(eventIDs) + ") AND ED_notification <> 'NONE';";
         ArrayList<Event> events = new ArrayList<>();
         try ( PreparedStatement stmt = con.prepareStatement(notNotified)) {
 
