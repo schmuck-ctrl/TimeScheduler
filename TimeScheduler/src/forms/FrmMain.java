97b516dde48a13fb5cd218c1970de4433180bd82
Joshua <joshu@192.168.0.248>
2021-01-27 16:34:49 +0100

    SetIcon

--- TimeScheduler/src/forms/FrmMain.java
+++ TimeScheduler/src/forms/FrmMain.java
@@ -12,6 +12,7 @@ import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
 import handlers.*;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Toolkit;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -53,6 +54,7 @@ public class FrmMain extends javax.swing.JFrame {
         eventHandler = new EventHandler();
         this.datePicker.getComponentDateTextField().setEditable(false);
         addDatePickerDateChangedEvent();
+        setIcon();
     }
 
     /**
@@ -493,4 +495,8 @@ public class FrmMain extends javax.swing.JFrame {
     private javax.swing.JPanel pnlMenuBar;
     private javax.swing.JSplitPane splitPnlContent;
     // End of variables declaration//GEN-END:variables
+
+    private void setIcon() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/icons/calendar-event-line.png")));
+    }
 }
